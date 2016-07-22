4f3c2e4d7814858a986064af1130a5a0bce2634b
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-22 15:41:01 -0500

    updated express routes

--- routes/assignments.js
+++ routes/assignments.js
@@ -13,10 +13,31 @@ router.get('/', function(request, response){
     }
   })
 });
-router.post('/addAssignment/:student_name/:assignment_name?', function(request, response){
+router.get('/:id', function(request, response){
+  Assignment.find({_id: request.params.id}, function(err, assignments){
+    if(err){
+      Assignment.find({}, function(err, assignments){
+        if(err){
+          console.log(err);
+          response.sendStatus(500);
+        }else {
+          response.send(assignments);
+        }
+      });
+    }else {
+      response.send(assignments);
+    }
+  });
+});
+
+router.post('/addAssignment', function(request, response){
   var createdAssignment = new Assignment({
-    student_name: request.params.student_name,
-    assignment_name: request.params.assignment_name,
+    assignment_number: request.body.assignment_number,
+    student_name: request.body.student_name,
+    assignment_name: request.body.assignment_name,
+    score: request.body.score,
+    date_completed: request.body.date_completed,
+    details: request.body.deta
   });
   createdAssignment.save(function(err){
     if(err){
