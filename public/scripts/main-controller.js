824ecf83b8d28990e0ad3755662c9e71a24c15fd
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-25 09:06:53 -0500

    id GET route

--- public/scripts/main-controller.js
+++ public/scripts/main-controller.js
@@ -7,26 +7,26 @@ angular.module('assignApp').controller('MainController', function($http){
   vm.assignment = {};
 
 
+
   vm.addAssignment = function(){
     console.log(vm.assignment);
     $http.post('/assignments/addAssignment', vm.assignment).then(function(response){
+      vm.assignment = {};
+      getAssignments();
       console.log(response);
     }, function(response){
       console.log('fail');
     }
   )
-
-
-
-
-
-
-
+}
+  var getAssignments = function(){
+    $http.get('/assignments').then(function(response){
+      console.log('success');
+      vm.assignments = response.data;
+    }, function(response){
+      console.log('err', response);
+    });
   }
-  $http.get('/assignments').then(function(response){
-    console.log('success');
-    vm.assignments = response.data;
-  }, function(response){
-    console.log('err', response);
-  });
+
+  getAssignments();
 })
